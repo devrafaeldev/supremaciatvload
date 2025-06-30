cd73bcf6b5c97f12f2d63dfd51c638f89bc7c0e0
devrafaeldev <gabrieladefreddiemercury@gmail.com>
2025-06-30 14:24:57 -0300

    Update index.js

--- api/h/index.js
+++ api/h/index.js
@@ -17,7 +17,15 @@ export default async function handler(req, res) {
     return res.status(400).send('URL inválida ou foi desligado.');
   }
 
-  const finalUrl = `https://lb1.embmaxtv.online/${targetUrl}/index.m3u8`;
+  // Lista de domínios possíveis
+  const domains = [
+    'https://lb1.embmaxtv.online',
+    'https://embmaxtv.online'
+  ];
+
+  // Seleciona um domínio aleatório
+  const randomDomain = domains[Math.floor(Math.random() * domains.length)];
+  const finalUrl = `${randomDomain}/${targetUrl}/index.m3u8`;
 
   res.setHeader('Content-Type', 'text/plain');
   res.send(finalUrl);
