f56118b6c921ea0e2cc99d7108c97bcfa03daffa
devrafaeldev <gabrieladefreddiemercury@gmail.com>
2025-06-30 14:16:05 -0300

    Create index.js

---
+++ api/h/index.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const origin = req.headers.origin;
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const targetUrl = req.url?.replace('/api/h/', '');
+  if (!targetUrl) {
+    return res.status(400).send('URL inválida ou foi desligado.');
+  }
+
+  const finalUrl = `https://lb1.embmaxtv.online/${targetUrl}/index.m3u8`;
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(finalUrl);
+}
